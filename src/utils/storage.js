75d2ae800f8b4d2ed6dff1632a959f3461f56e6e
snipexytkox-prog <snipexytkox@gmail.com>
2026-09-16 07:05:19 +0200

    Update storage.js

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,1 +1,38 @@
+const fs = require('fs');
+const path = require('path');
 
+const filePath = path.join(__dirname, 'database.json');
+
+function getData() {
+    try {
+        if (!fs.existsSync(filePath)) return {};
+        return JSON.parse(fs.readFileSync(filePath, 'utf8'));
+    } catch (e) {
+        return {};
+    }
+}
+
+function saveData(data) {
+    try {
+        fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
+    } catch (e) {}
+}
+
+const storage = {
+    get(key) {
+        const data = getData();
+        return data[key];
+    },
+    set(key, value) {
+        const data = getData();
+        data[key] = value;
+        saveData(data);
+    },
+    delete(key) {
+        const data = getData();
+        delete data[key];
+        saveData(data);
+    }
+};
+
+module.exports = storage;
