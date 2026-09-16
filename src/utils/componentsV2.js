7557b7cc69e3b75019b318577a792fdaa66a6e53
snipexytkox-prog <snipexytkox@gmail.com>
2026-09-16 07:02:39 +0200

    Update componentsV2.js

--- src/utils/componentsV2.js
+++ src/utils/componentsV2.js
@@ -1,1 +1,2 @@
-
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle, StringSelectMenuBuilder, ModalBuilder, TextInputBuilder, TextInputStyle, EmbedBuilder } = require('discord.js');
+module.exports = { ActionRowBuilder, ButtonBuilder, ButtonStyle, StringSelectMenuBuilder, ModalBuilder, TextInputBuilder, TextInputStyle, EmbedBuilder };
