0f8ce5e2e1cac530bbf5cee0e77906c89d58acd5
snipexytkox-prog <snipexytkox@gmail.com>
2026-09-16 07:39:12 +0200

    Update componentsV2.js

--- src/utils/componentsV2.js
+++ src/utils/componentsV2.js
@@ -1,2 +1,36 @@
-const { ActionRowBuilder, ButtonBuilder, ButtonStyle, StringSelectMenuBuilder, ModalBuilder, TextInputBuilder, TextInputStyle, EmbedBuilder } = require('discord.js');
-module.exports = { ActionRowBuilder, ButtonBuilder, ButtonStyle, StringSelectMenuBuilder, ModalBuilder, TextInputBuilder, TextInputStyle, EmbedBuilder };
+// Przykład struktury pliku src/utils/componentsV2.js
+
+const { TextDisplayBuilder, SeparatorBuilder, ButtonBuilder } = require('discord.js');
+
+function createHeader(text, emoji) {
+  return new TextDisplayBuilder().setContent(`### ${emoji} ${text}`);
+}
+
+function createSeparator() {
+  return new SeparatorBuilder();
+}
+
+function createSectionWithButton(text, buttonLabel, customId, style, emoji) {
+  // Twoja implementacja sekcji z przyciskiem
+}
+
+function createLogoMediaGallery() {
+  // Twoja implementacja galerii logo
+}
+
+function getLogoAttachment() {
+  // Zwracanie załącznika
+}
+
+function createFooter(text) {
+  return new TextDisplayBuilder().setContent(`-* ${text}`);
+}
+
+module.exports = {
+  createSeparator,
+  createHeader,
+  createSectionWithButton,
+  createLogoMediaGallery,
+  getLogoAttachment,
+  createFooter
+};
